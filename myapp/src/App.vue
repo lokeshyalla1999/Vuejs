cbba3d974b7a486cb7d7d605165fcea81d5a43b7
LY65822 <LY65822@DERIW46ZLPC0368>
2022-07-11 13:33:40 +0530

    commit

---
+++ myapp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <BindingAll /> 
+ 
+</template>
+
+<script>
+import BindingAll from './components/BindingAll.vue'
+export default {
+  name: 'App',
+  components: {
+    BindingAll
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ myapp/src/components/BindingAll.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Binding</h1>
+     <p>Binding are three types they are 1.binding Text 2.Binding Html 3.Binding attributes</p>
+
+     <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'BindingAll',
+    data(){
+        return{
+            name : 'chitti'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
